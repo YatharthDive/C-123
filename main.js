630ad52d462fcc4ca6ec7af4b0725a049d6a0cc6
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-07-29 14:38:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-function setup(){
-    canvas=createCanvas(300,300);
-    canvas.position(550,150)
+function setup() {
+    canvas = createCanvas(550, 500);
+    canvas.position(600, 90)
+    video = createCapture(VIDEO)
+    video.size(550, 500);
+    posenet = ml5.poseNet(video, modelLoaded)
+    posenet.on("pose", gotposes)
 }
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
+}
+
+function modelLoaded() {
+    console.log("poseNet is initalized");
+}
+
+function draw() {
+    fill("red");
+    textSize(20);
+    console.log("Yatharth");
+    text("Yatharth",200,200)
+}
+
